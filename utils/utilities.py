33be06c0733e4217b7b3c51d548747642b6da7a6
Artur <86047011+arkturw@users.noreply.github.com>
2021-08-27 11:20:55 +0200

    Poprawione przechwytywanie wyjątków w utilsach

--- utils/utilities.py
+++ utils/utilities.py
@@ -1,19 +1,18 @@
-from selenium.common import exceptions
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException
+)
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def wait_for_alert(driver:object, seconds:int=5):
+def wait_for_alert(driver: object, seconds: int = 5):
     try:
         wait = WebDriverWait(driver, seconds)
         wait.until(EC.alert_is_present())
         return True
-    except exceptions.NoSuchElementException as e:
-        print('Wystąpił wyjątek: ', e)
-        return False
-    except exceptions.TimeoutException as e:
-        print('Wystąpił wyjątek: ', e)
-        return False
-    except Exception as e:
+    except (NoSuchElementException,
+            TimeoutException,
+            Exception) as e:
         print('Wystąpił wyjątek: ', e)
         return False
